d2c371e55cd4f2fb022ad6f104478fda87c47c4a
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-02 15:24:00 -0700

    setup for Heroku

---
+++ frontend/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
+
+const store = configureStore();
+  if (process.env.NODE_ENV !== 'production') {
+    window.store = store;
+    // window.csrfFetch = csrfFetch;
+  }
+
+  function Root() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    )
+  }
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+renderApplication();
+
+// if (sessionStorage.getItem('X-CSRF-Token') === null) {
+//   restoreCSRF().then(renderApplication);
+// } else {
+//   renderApplication();
+// }
+
