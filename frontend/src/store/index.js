920103fe27f4622b23acceab0ddd49e78cae8de7
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-02 17:12:25 -0700

    config heroku

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,7 +7,7 @@ const rootReducer = combineReducers({
 
 let enhancer;
 
-if (process.env.Node_env === 'production') {
+if (process.env.NODE_ENV === 'production') {
     enhancer = applyMiddleware(thunk);
 } else {
     const logger = require('redux-logger').default;
