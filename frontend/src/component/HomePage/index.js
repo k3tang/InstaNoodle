d8c3d8f5ca72fa2d811768139177b248ef0bad26
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-05 19:59:29 -0700

    add css to splash page

--- frontend/src/component/HomePage/index.js
+++ frontend/src/component/HomePage/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./HomePage.css";
 import { Link } from "react-router-dom";
-import splashPic from '../../assets/splash-pic.jpg';
+import splashPic from '../../assets/splash-pic.png';
 
 const HomePage = () => {
   
     return (
         <>  
+        <div id="splash-page">
             <div id="splash-text">
                 <h1>
                     NUTRITIONALLY COMPLETE INSTANT NOODLES
@@ -14,11 +15,15 @@ const HomePage = () => {
                 <h2>
                     Plant-based instant noodles that are healthy for you and our planet.
                 </h2>
-                <Link to="/products">Check it out</Link>
+                <p>
+                    <a href="/products">Check it out</a>
+                </p>
+                
             </div>
             <div id="splash-pic">
-                <img src={splashPic} alt= "noodles in bowl"/>
+                <img src={splashPic} alt= "noodles in bowl" />
             </div>
+        </div>
         </>
        
     )
