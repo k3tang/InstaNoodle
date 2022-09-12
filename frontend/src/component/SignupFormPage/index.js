eb13532eaf498058e2bd8b79b640d91fd47a4e0a
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-12 15:34:19 -0700

    heroku config

--- frontend/src/component/SignupFormPage/index.js
+++ frontend/src/component/SignupFormPage/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
-import './signupform.css';
+import './index.css';
 import cartoon from "../../assets/login-image.jpg"
 import { fetchCartItems } from "../../store/cart";
 
