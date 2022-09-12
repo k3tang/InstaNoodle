4fe2631a2903dfe6af09ee31ab632891d2b1c2fc
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-12 15:31:13 -0700

    heroku config

--- frontend/src/component/LoginFormPage/index.js
+++ frontend/src/component/LoginFormPage/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import './loginform.css';
+import './index.css';
 import {NavLink} from 'react-router-dom';
 import cartoon from "../../assets/login-image.jpg";
 import { fetchCartItems } from '../../store/cart';
