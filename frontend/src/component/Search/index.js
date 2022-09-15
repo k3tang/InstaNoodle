243d29a0d5e9f1ac168248e13e32f1c47ae333ec
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-15 12:51:56 -0700

    push to heroku

--- frontend/src/component/Search/index.js
+++ frontend/src/component/Search/index.js
@@ -10,6 +10,7 @@ const SearchBar = () => {
     const dispatch = useDispatch();
     const history = useHistory();
     const [query, setQuery] = useState("");
+    const [showModal, setShowModal]
 
     const handleClick = (e) => {
         e.preventDefault();
---
+++ frontend/src/component/Search/searchmodal.js
@@ -0,0 +1,8 @@
+
+const SearchModal = () => {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+export default SearchModal;
